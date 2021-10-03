ba7ba37119b01a9095b196bb2fe8e198a7f6e5c6
Ava Drumm <lmbdrumm@gmail.com>
2021-10-03 16:29:18 +0200

    Added main user menu

--- src/routes/user/[api_url]/index.svelte
+++ src/routes/user/[api_url]/index.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+	import axios from 'axios';
+	import { page } from '$app/stores';
+	import HoleCard from './_HoleCard.svelte';
 
+	var api_url: string;
+	api_url = `http://${$page.params.api_url}/user/ALL`;
+
+	async function fetch() {
+		try {
+			const response = await axios.get(api_url);
+			//await sleep(500); //this is just so that I can see what the loading screen looks like
+			//console.log(response.data)
+			return response.data;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
+
+	var database = fetch();
 </script>
 
 <head>
@@ -9,3 +27,21 @@
 <h1 class="generic-title" style="text-align: center;">
 	Vælg et hul her:
 </h1>
+
+{#await database}
+	<p>
+		loading...
+	</p>
+{:then database} 
+
+	<div class="hole-card-grid">
+		{#each database as db}
+			<HoleCard hole={db.hole} db_id={db.db_id}/>
+		{/each}
+	</div>
+
+{:catch error}
+	<p>
+		{error}
+	</p>
+{/await}
