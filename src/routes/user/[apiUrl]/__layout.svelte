d11d06ce1478725b4ae5491a7856e5fba842654b
Ava Drumm <lmbdrumm@gmail.com>
2022-05-02 12:52:47 +0200

    Modal finished

--- src/routes/user/[apiUrl]/__layout.svelte
+++ src/routes/user/[apiUrl]/__layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { page } from '$app/stores';
     import SvgIcon from '@jamescoyle/svelte-icon';
-    import { mdiArrowLeft, mdiInformationOutline } from '@mdi/js';
+    import { mdiArrowLeft, mdiInformationOutline, mdiGithub } from '@mdi/js';
     import Modal from './_components/modal.svelte';
+    import { VERSION } from '../../../../static/components/stores';
 
     let infoOpen: boolean = false;
 </script>
@@ -22,8 +23,33 @@
 <body>
     <Modal bind:open={infoOpen}>
         <h1>
-            infofelt
+            Du bruger BullseyeGolf version: {$VERSION}
         </h1>    
+        <p>
+			Programmet er stadig under udvikling, så hvis du opdager nogle problemer eller har ideer til
+			nye funktioner ville det blive værdsat hvis du laver en 'issue' på denne bug tracker
+        </p>
+        <a href="https://github.com/itzgoldenleonard/BullseyeGolf-frontend/issues" target="_blank">
+            <div>
+                <SvgIcon type="mdi" path={mdiGithub}/>
+                <p>Bug tracker</p>
+            </div>
+        </a>
+		<p>
+            BullseyeGolf er fri/gratis software og bruger AGPL-v3 licensen. Du kan se kildekoden her:
+        </p>
+        <a href="https://github.com/itzgoldenleonard/BullseyeGolf-frontend" target="_blank">
+            <div>
+                <SvgIcon type="mdi" path={mdiGithub}/>
+                <p>Kildekode til frontend</p>
+            </div>
+        </a>
+        <a href="https://github.com/itzgoldenleonard/BullseyeGolf-backend" target="_blank">
+            <div>
+                <SvgIcon type="mdi" path={mdiGithub}/>
+                <p>Kildekode til backend</p>
+            </div>
+        </a>
     </Modal>
     <slot></slot>
 </body>
@@ -37,6 +63,25 @@
 		width: auto;
 		padding: $padding;
         overflow: hidden;
+    }
+
+    a {
+        color: $text-color;
+        text-decoration: none;
+        
+        div {
+            @include button();
+            margin: $padding 0;
+            padding-left: $padding-small;
+            display: flex;
+            align-items: center;
+            width: auto;
+
+            
+            > * {
+                margin-left: $padding-small;
+            }
+        }
     }
 
     header {
@@ -58,7 +103,6 @@
         }
 
         a {
-            color: $text-color;
             position: absolute;
             left: 0;
             top: 0;
