12962eddef127d762d98b9ef407b1c60d00cfe1e
Ava Drumm <lmbdrumm@gmail.com>
2022-05-06 16:56:19 +0200

    Submit button somewhat working

--- src/routes/user/[apiUrl]/[dbId]/[holeNumber].svelte
+++ src/routes/user/[apiUrl]/[dbId]/[holeNumber].svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { getHole } from '../scripts/api';
-	let baseUrl: string = `https://${$page.params.apiUrl}/user/`;
+	import { getHole, submitScore } from '../scripts/api';
+	import InputText from '../../../../../static/components/InputText.svelte';
+	import InputNumber from '../../../../../static/components/InputNumber.svelte';
+	import Modal from '../_components/Modal.svelte';
     import Score from '../_components/Score.svelte';
 
+	let baseUrl: string = `https://${$page.params.apiUrl}/user/`;
 	let hole = getHole(baseUrl, $page.params.dbId, Number($page.params.holeNumber));
+	let submitting: boolean = false;
+
+	let name: string = '';
+	let scoreM: number = 0;
+	let scoreCm: number = 0;
+
+	async function submit() {
+		await submitScore(baseUrl, $page.params.dbId, Number($page.params.holeNumber), name, scoreM, scoreCm);
+	}
 </script>
 
 {#await hole}
@@ -39,7 +51,30 @@
                 Der er ingen noteringer endnu <br> Vær den første til at indsende en
             </h2>
         {/each}
+
+		<button on:click={() => submitting = true}>
+			Indsend notering
+		</button>
     </main>
+	
+	<Modal bind:open={submitting}>
+		<h1>
+			Indsend notering
+		</h1>
+
+		<form on:submit|preventDefault={submit}>
+			<InputText label="Navn" bind:value={name} maxlength={40} required/>
+		
+			<div>
+				<InputNumber label="Distance" bind:value={scoreM} min={0} max={10} unit="m" required/>
+				<InputNumber label="" bind:value={scoreCm} min={0} max={99} unit="cm" required/>
+			</div>
+
+			<button>
+				Indsend
+			</button>
+		</form>
+	</Modal>
 {:catch error}
 	<p>{error}</p>
 {/await}
@@ -58,4 +93,21 @@
         grid-template-rows: repeat(6, 1fr);
         gap: $padding;
     }
+
+	button {
+		@include button($primary-color);
+		//text-shadow: $shadow-text;
+	}
+
+	form {
+		display: grid;
+		gap: $padding;
+		width: 90vw;
+		
+		div {
+			display: grid;
+			gap: $padding;
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
 </style>
--- src/routes/user/[apiUrl]/scripts/api.ts
+++ src/routes/user/[apiUrl]/scripts/api.ts
@@ -11,8 +11,8 @@ export async function getTournamentList(url: string): Promise<ShortTournament[]>
 
 export async function getTournament(baseUrl: string, db_id: string): Promise<Tournament> {
 	try {
-		const request_url: string = `${baseUrl}/${db_id}`;
-		const response = await axios.get(request_url);
+		const requestUrl: string = `${baseUrl}/${db_id}`;
+		const response = await axios.get(requestUrl);
 		response.data.db_id = db_id;
 		return response.data;
 	} catch (error) {
@@ -22,8 +22,23 @@ export async function getTournament(baseUrl: string, db_id: string): Promise<Tou
 
 export async function getHole(baseUrl: string, db_id: string, hole_number: number): Promise<Hole> {
 	try {
-		const request_url: string = `${baseUrl}/${db_id}/${hole_number}`;
-		const response = await axios.get(request_url);
+		const requestUrl: string = `${baseUrl}/${db_id}/${hole_number}`;
+		const response = await axios.get(requestUrl);
+		return response.data;
+	} catch (error) {
+		throw new Error(error);
+	}
+}
+
+export async function submitScore(baseUrl: string, db_id: string, hole_number: number, name: string, scoreM: number, scoreCm: number): Promise<any> {
+	const score: number = scoreM + scoreCm * 0.01;
+	const requestUrl: string = `${baseUrl}/${db_id}/${hole_number}`;
+	
+	try {
+		const response = await axios.post(requestUrl, {
+			player_name: name,
+			player_score: score
+		});
 		return response.data;
 	} catch (error) {
 		throw new Error(error);
