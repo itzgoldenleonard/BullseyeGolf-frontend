715d7e0bc5ad7417df0df0bd9e0a1af17afbf1ca
Ava Drumm <lmbdrumm@gmail.com>
2022-06-26 12:35:00 +0200

    Finalized scorelist colors

--- src/routes/u/[username]~[apiUrl]/_components/ScoreList.svelte
+++ src/routes/u/[username]~[apiUrl]/_components/ScoreList.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
-    import { displayScore } from '$lib/displayScore';
+    // SMUI Components
 	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 	import LinearProgress from '@smui/linear-progress';
 
+    // Variables
 	export let scores: Score[];
 	export let loaded: boolean;
+
+    // Functions
+    import { displayScore } from '$lib/displayScore';
 </script>
 
 <DataTable style="width: 100%; max-height: inherit;">
 	<Head>
 		<Row>
 			<Cell>Nr.</Cell>
-			<Cell style="width: 100%">Navn</Cell>
+            <Cell style="width: 100%">Navn</Cell>
 			<Cell>Score</Cell>
 		</Row>
 	</Head>
@@ -32,41 +36,3 @@
 		slot="progress"
 	/>
 </DataTable>
-
-<style lang="scss">
-	:global(.score-0) {
-        outline: 5px solid hsl(50, 85%, 60%);
-        outline-offset: -5px;
-        //background: linear-gradient(137deg, hsl(50, 85%, 60%) 0%, 50%, hsl(45, 100%, 33%));
-        /*
-		background-image: linear-gradient(137deg, hsl(50, 85%, 60%) 0%, 50%, hsl(45, 100%, 33%));
-		background-size: 100% 10px;
-		background-position: 0 100%;
-		background-repeat: no-repeat;
-        */
-	}
-
-	:global(.score-1) {
-        outline: 5px solid hsl(225, 9%, 79%);
-        outline-offset: -5px;
-        //background: linear-gradient(142deg, hsl(225, 9%, 79%) 0%, 50%, hsl(225, 6%, 50%));
-        /*
-		background-image: linear-gradient(142deg, hsl(225, 9%, 79%) 0%, 50%, hsl(225, 6%, 50%));
-		background-size: 100% 10px;
-		background-position: 0 100%;
-		background-repeat: no-repeat;
-        */
-	}
-
-	:global(.score-2) {
-        outline: 5px solid hsl(25, 64%, 65%);
-        outline-offset: -5px;
-        //background: linear-gradient(150deg, hsl(25, 64%, 65%) 0%, 40%, hsl(20, 67%, 38%));
-        /*
-		background-image: linear-gradient(150deg, hsl(25, 64%, 65%) 0%, 40%, hsl(20, 67%, 38%));
-		background-size: 100% 10px;
-		background-position: 0 100%;
-		background-repeat: no-repeat;
-        */
-	}
-</style>
