0cf1ebebfb731d1c1835b42659979ca09ee7e0e2
Ava Drumm <lmbdrumm@gmail.com>
2022-07-22 11:44:04 +0200

    Finished tournaments show up in user panel if theyre less than 3 days old

--- src/routes/u/[username]~[apiUrl]/index.svelte
+++ src/routes/u/[username]~[apiUrl]/index.svelte
@@ -7,6 +7,11 @@
 	let tournamentList = getTournamentList(`https://${$page.params.apiUrl}/${$page.params.username}`);
 	// Functions
 	import { getTournamentList } from './scripts/api';
+
+    function finishedTournamentsFilter(e: shortTournament): boolean {
+        let days = 3;
+        return !e.active && Date.now() / 1000 <= e.t_end + 86400 * days && e.t_start <= Date.now() / 1000;
+    }
 </script>
 
 <h2>Vælg en turnering</h2>
@@ -25,6 +30,20 @@
 		{:else}
 			Der er ingen aktive turneringer
 		{/each}
+        {#if tournamentList.filter(finishedTournamentsFilter).length !== 0}
+            <p>Afsluttede turneringer</p>
+            {#each tournamentList.filter(finishedTournamentsFilter) as tournament}
+                <Card>
+                    <PrimaryAction
+                        padded
+                        on:click={() => (location.href = `${$page.url.pathname}/${tournament.tournament_id}`)}
+                        style="text-align: center; font-family: Roboto;"
+                    >
+                        {tournament.tournament_name}
+                    </PrimaryAction>
+                </Card>
+            {/each}
+        {/if}
 	{:catch error}
 		<p>{error}</p>
 	{/await}
